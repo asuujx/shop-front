b27f27377f8ac486388931b0b154457736964277
asuujx <43821373+asuujx@users.noreply.github.com>
2024-12-18 00:49:27 +0100

    feature: Add website logo

--- src/components/header/elements/Logo.tsx
+++ src/components/header/elements/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 function Logo() {
   return (
     <Link to="/" className="font-bold">
-      Placeholder
+      <img src="/logo.png" alt="logo" className="w-16" />
     </Link>
   );
 }
