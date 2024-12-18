d3460b20aa3d6d07282aad8f269cbe61e2e21627
asuujx <43821373+asuujx@users.noreply.github.com>
2024-12-18 21:36:32 +0100

    fix: Fix page size in multiple places

--- src/pages/user/User.tsx
+++ src/pages/user/User.tsx
@@ -4,7 +4,7 @@ import PersonalInformationCard from "@/components/user/PersonalInformationCard";
 
 function User() {
   return (
-    <div className="w-screen h-screen">
+    <div>
       <div className="max-w-screen-xl mx-4 lg:mx-auto flex flex-col items-center justify-center">
         <h1 className="mt-10 ml-5 text-4xl font-semibold">
           Informacje o koncie
