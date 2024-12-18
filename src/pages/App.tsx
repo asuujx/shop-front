58b397dbcc1dcd6912c5d1a2325dbd26499aa4bd
asuujx <43821373+asuujx@users.noreply.github.com>
2024-12-18 21:29:51 +0100

    fix: Fix page size to screen

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <div className="flex flex-col">
+    <div className="w-screen h-screen flex flex-col">
       <Header />
       <Outlet />
     </div>
