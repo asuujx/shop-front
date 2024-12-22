d6b21d790dbadcb3d14cdf8d9e8189a8d3ae3e36
asuujx <43821373+asuujx@users.noreply.github.com>
2024-12-22 16:58:04 +0100

    feature: Add ProductsList page

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -4,17 +4,24 @@ import Login from "@/pages/auth/Login";
 import ResetPassword from "@/pages/auth/ResetPassword";
 import SignUp from "@/pages/auth/SignUp";
 import Verify from "@/pages/auth/Verify";
+import Home from "@/pages/home/Home";
 import CreateOffer from "@/pages/offers/CreateOffer";
 import Offer from "@/pages/offers/Offer";
 import OffersList from "@/pages/offers/OffersList";
+import Product from "@/pages/products/ProductPage";
+import ProductsList from "@/pages/products/ProductsList";
 import User from "@/pages/user/User";
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
       {
         path: "signup",
         element: <SignUp />,
@@ -36,16 +43,34 @@ export const router = createBrowserRouter([
         element: <User />,
       },
       {
-        path: "offers/",
-        element: <OffersList />,
+        path: "offers",
+        element: <Outlet />,
         children: [
+          {
+            index: true,
+            element: <OffersList />,
+          },
           {
             path: "create",
             element: <CreateOffer />,
           },
           {
             path: ":id",
-            element: <Offer />
+            element: <Offer />,
+          },
+        ],
+      },
+      {
+        path: "products",
+        element: <Outlet />,
+        children: [
+          {
+            index: true,
+            element: <ProductsList />,
+          },
+          {
+            path: ":id",
+            element: <Product />,
           },
         ],
       },
